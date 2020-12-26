01540fcb38ab5c0ce04a64b0b82ded31d210d3d5
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-12-26 18:22:40 +0200

    add fetch logic

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -9,16 +9,59 @@ const searchQuery = _.debounce(() => {
 
 function fetchCountries(searchQuery) {
   if (searchQuery === '') {
+    clearUl();
     return;
   }
   fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
     .then(response => {
       return response.json();
     })
-    .then(data => data.map(data => data.name))
-    .then(console.log);
+    .then(data =>
+      data.filter(country =>
+        country.name
+          .toLowerCase()
+          .includes(refs.formCountryNameInput.value.toLowerCase()),
+      ),
+    )
+    .then(countriesArray => {
+      if (countriesArray.length > 1 && countriesArray.length <= 10) {
+        countriesArray.map(({ name }) => {
+          multipleRender(name);
+        });
+      } else if (countriesArray.length === 1) {
+        countriesArray.map(({ name, capital, population, languages, flag }) => {
+          singleRender(name, capital, population, languages, flag);
+        });
+      }
+    });
+}
+
+function multipleRender(name) {
+  refs.searchResult.insertAdjacentHTML('beforeend', `<li>${name}</li>`);
+}
+
+function singleRender(name, capital, population, languages, flag) {
+  refs.searchResult.insertAdjacentHTML(
+    'beforeend',
+    `<li>
+        <ul>
+          <li>${name}</li>
+          <li>${capital}</li>
+          <li>${population}</li>
+          <li>${languages}</li>
+          <li><img src="${flag}" alt="${name}"></li>
+        </ul>
+      </li>`,
+  );
+}
+
+function clearUl() {
+  if (refs.formCountryNameInput.value === '') {
+    refs.searchResult.innerHTML = '';
+  }
 }
 
 refs.formCountryNameInput.addEventListener('input', searchQuery);
+refs.formCountryName.addEventListener('submit', e => e.preventDefault());
 
 export default fetchCountries;
